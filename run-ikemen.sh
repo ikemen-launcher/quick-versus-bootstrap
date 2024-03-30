68dc5e5bb1478d02857728df88eabfed7e8bf54d
neolao <contact@neolao.com>
2024-03-30 22:06:22 +0100

    up

---
+++ run-ikemen.sh
@@ -0,0 +1,85 @@
+#/bin/bash
+
+currentDirectory=$(dirname "$(realpath "$0")")
+cd "$currentDirectory/Ikemen_GO"
+
+configPath="../save/config.json"
+motifDirectory="../motifs"
+lifebarDirectory="../lifebars"
+stageDirectory="../stages"
+characterDirectory="../chars"
+
+motif="system.def"
+lifebar="VHD/fight.def"
+rounds=2
+characterOneColor=1
+characterTwoColor=1
+characterTwoAI=0
+
+for i in "$@"; do
+  case $i in
+    -motif=*)
+      motif="${i#*=}"
+      shift
+      ;;
+    -lifebar=*)
+      lifebar="${i#*=}"
+      shift
+      ;;
+    -p1=*)
+      characterOne="${i#*=}"
+      shift
+      ;;
+    -p1.color=*)
+      characterOneColor="${i#*=}"
+      shift
+      ;;
+    -p2=*)
+      characterTwo="${i#*=}"
+      shift
+      ;;
+    -p2.color=*)
+      characterTwoColor="${i#*=}"
+      shift
+      ;;
+    -p2.ai=*)
+      characterTwoAI="${i#*=}"
+      shift
+      ;;
+    -s=*)
+      stage="${i#*=}"
+      shift
+      ;;
+    -rounds=*)
+      rounds="${i#*=}"
+      shift
+      ;;
+    *)
+      ;;
+  esac
+done
+
+case "$OSTYPE" in
+  darwin*)
+    ikemen="./Ikemen_GO_MacOS -AppleMagnifiedMode YES"
+  ;;
+  linux*)
+    ikemen="./Ikemen_GO_Linux"
+  ;;
+  *)
+    ikemen="./Ikemen_GO_Linux"
+  ;;
+esac
+
+$ikemen \
+  -config "$configPath" \
+  -r "$motifDirectory/$motif" \
+  -lifebar "$lifebarDirectory/$lifebar" \
+  -s "$stageDirectory/$stage" \
+  -rounds $rounds \
+  -p1 "$characterDirectory/$characterOne" \
+  -p1.color $characterOneColor \
+  -p2 "$characterDirectory/$characterTwo" \
+  -p2.color $characterTwoColor \
+  -p2.ai $characterTwoAI
+
